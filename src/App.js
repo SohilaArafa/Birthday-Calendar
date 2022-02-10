4b7f9ff5082b0f36e8daf15873c7f27f5e687cd9
SohilaArafa <sohilaarafa2001@gmail.com>
2022-02-10 17:51:55 +0200

    final

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import data from './data';
 import List from './List';
 function App() {
   const [people,setPeople] = useState(data);
+  const clearBirthdays = () => {
+      setPeople([]);
+  }
   return (
     <section className='container'>
       <h3>{people.length} birthdays today</h3>
       <List people={people}/>
-      <button type='button' onClick={() => console.log('you clicked me')}>
+      <button type='button' onClick={() => clearBirthdays() }>
         Clear Birthdays
         </button>
     </section>

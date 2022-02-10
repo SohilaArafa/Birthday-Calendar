89d4874f5b035daa8f940c7458f72216a416544c
SohilaArafa <sohilaarafa2001@gmail.com>
2022-02-10 17:41:18 +0200

    adding count of birthdays we have

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people,setPeople] = useState(data);
   return (
     <section className='container'>
-      <h3>0 birthdays today</h3>
-      <List />
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
       <button type='button' onClick={() => console.log('you clicked me')}>
         Clear Birthdays
         </button>
